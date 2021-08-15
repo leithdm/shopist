41721ba3098972ef6b632dfe5ed703cbdb02cb4b
leithdm@gmail.com <leithdm@gmail.com>
2021-08-15 21:02:04 +0100

    feat: Add Category and Product models. Render dummy products to display on  home page

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.store, name="home"),
+    path('', views.home, name="home"),
+    path('<slug:category_slug>', views.home, name="categorized_products"),
     path('product/', views.productDetails, name="product"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
 # Create your views here.
-def store(request):
-    return render(request, 'store/index.html')
+def home(request, category_slug=None):
+    category_page = None
+
+    # if there is a category slug, filter the home page to show all products of that category
+    if category_slug != None:
+        category_page = get_object_or_404(Category, slug=category_slug)
+        # find all products that belong to a category_page
+        products = Product.objects.filter(category=category_page, available=True)
+    else:
+        # if the category field is empty, display all available products
+        products = Product.objects.all().filter(available=True)
+
+    return render(request, 'store/index.html', {'category': category_page, 'products': products})
 
 
 def productDetails(request):
