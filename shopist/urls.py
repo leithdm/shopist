4e43a160f7dd0fbb5bf68a920f7b26e6d2d99124
leithdm@gmail.com <leithdm@gmail.com>
2021-08-19 20:49:02 +0100

    feat: Add checkout app and create a basic index.html page

--- shopist/urls.py
+++ shopist/urls.py
@@ -18,9 +18,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
+    path('cart/', include('checkout.urls')),
+
 ] 
 
 if settings.DEBUG:
--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,5 @@ from .import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('category/<slug:category_slug>', views.home, name="categorized_products"),
-    path('category/<slug:category_slug>/<slug:product_slug>', views.productDetails, name="product_detail"),
+    path('category/<slug:category_slug>/<slug:product_slug>', views.productDetails, name="product_detail")
 ]
