d4db8cb1f204c8b7339ab252a80a889a082b8626
leithdm@gmail.com <leithdm@gmail.com>
2021-08-22 17:16:51 +0100

    feat: Update the Admin page to show more details related to category and product items

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from .models import Category, Product
 
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(Product)
+
+#customize category fields shown on the admin table
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    #pre-populate the slug based on the name field
+    prepopulated_fields = {'slug':('name',)}
+
+#customize product fields shown on the admin table
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'stock', 'available', 'description', 'created', 'updated']
+    list_editable = ['price', 'stock', 'description', 'available']
+    prepopulated_fields = {'slug':('name',)}
+    #automate pagination
+    list_per_page = 25
+
+
+#site registration
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
