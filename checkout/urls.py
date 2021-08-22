8806cb848a512a4a7524709fde655f0673784ecd
leithdm@gmail.com <leithdm@gmail.com>
2021-08-22 14:44:24 +0100

    feat: Add ability to use trash icon to delete product from cart

--- checkout/urls.py
+++ checkout/urls.py
@@ -5,5 +5,7 @@ from .import views
 urlpatterns = [
     path('', views.cart_detail, name='cart_detail'),
     path('add/<int:product_id>', views.add_cart, name='add_cart'),
+    path('remove/<int:product_id>', views.cart_removeItem, name='cart_removeItem'),
+    path('trash/<int:product_id>', views.cart_trashItem, name='cart_trashItem'),
 
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from store.models import Category, Product
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
 
-# Create your views here.
-# def cart(request):
-#     return render(request, 'checkout/index.html')
 
-
+''' Get the cart_id '''
 def get_cart_id(request):
     cart = request.session.session_key
     if not cart:
@@ -15,7 +12,7 @@ def get_cart_id(request):
     return cart
 
 
-# Add a product to the cart, and update its quantity
+''' Add a product to the cart, and update its quantity '''
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)
     try:
@@ -48,7 +45,7 @@ def add_cart(request, product_id):
     return redirect('cart_detail')
 
 
-# Will retrieve all cartItems in the current session, and calculate total cost of all cart items in the cart
+''' Retrieve all cartItems in the current session, and calculate total cost of all cart items in the cart'''
 def cart_detail(request, total=0, counter=0, cart_items=None):
     # try to get the Cart object from the current session
     try:
@@ -59,9 +56,32 @@ def cart_detail(request, total=0, counter=0, cart_items=None):
             total += (cart_item.product.price * cart_item.quantity)
             # total number of items in the cart
             counter += cart_item.quantity
-    
     # if no cart is present in the session, we ignore it
     except ObjectDoesNotExist:
         pass
-
     return render(request, 'checkout/index.html', dict(cart_items=cart_items, total=total, counter=counter))
+
+
+''' Remove a cartItem from the cart '''
+def cart_removeItem(request, product_id):
+    cart = Cart.objects.get(cart_id=get_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    #if qty is >1, then decrement by 1
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    #if qty=0, remove it from the cart
+    else:
+        cart_item.delete()
+    #redirect to 'cart_detail' to update the UI
+    return redirect('cart_detail')
+
+
+''' Trash a cartItem from the cart '''
+def cart_trashItem(request, product_id):
+    cart = Cart.objects.get(cart_id=get_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart_detail')
