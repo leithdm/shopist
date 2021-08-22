a8cd406089e40d284d88a1c648540c3995d069ad
leithdm@gmail.com <leithdm@gmail.com>
2021-08-22 17:28:41 +0100

    feat: Add Order and OrderItem models, along with  migrations

--- checkout/models.py
+++ checkout/models.py
@@ -2,14 +2,16 @@ from django.db import models
 from django.urls import reverse
 from store.models import Product, Category
 
-# Create your models here.
+
 class Cart(models.Model):
     cart_id = models.CharField(max_length=250, blank=True)
     date_added = models.DateField(auto_now_add=True)
 
+
     class Meta:
         db_table = 'Cart'
         ordering = ['date_added']
+
 
     def __str__(self):
         return self.cart_id
@@ -21,11 +23,60 @@ class CartItem(models.Model):
     quantity = models.IntegerField()
     active = models.BooleanField(default=True)
 
+
     class Meta:
         db_table = 'CartItem'
 
+
     def sub_total(self):
         return self.product.price * self.quantity
+
+
+    def __str__(self):
+        return self.product
+
+
+class Order(models.Model):
+    token = models.CharField(max_length=250, blank=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='EUR Order Total')
+    emailAddress = models.EmailField(max_length=250, blank=True, verbose_name='Email Address')
+    created = models.DateTimeField(auto_now_add=True)
+    billingName = models.CharField(max_length=250, blank=True)
+    billingAddress1 = models.CharField(max_length=250, blank=True)
+    billingCity = models.CharField(max_length=250, blank=True)
+    billingPostcode = models.CharField(max_length=250, blank=True)
+    billingCountry = models.CharField(max_length=250, blank=True)
+    shippingName = models.CharField(max_length=250, blank=True)
+    shippingAddress1 = models.CharField(max_length=250, blank=True)
+    shippingCity = models.CharField(max_length=250, blank=True)
+    shippingPostcode = models.CharField(max_length=250, blank=True)
+    shippingCountry = models.CharField(max_length=250, blank=True)
+
+
+    class Meta:
+        db_table = 'Order'
+        ordering = ['-created']
+
+
+    def __str__(self):
+        return str(self.id)
+
+
+#Each order will include multiple order items   
+class OrderItem(models.Model):
+    product = models.CharField(max_length=250)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='EUR Price')
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'OrderItem'
+
+
+    #calculates sub-total of qty items
+    def sub_total(self):
+        return self.quantity * self.price
+
 
     def __str__(self):
         return self.product
