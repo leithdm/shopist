1bec6196a2a80b05bea9ada5cf2619391b5e2548
leithdm@gmail.com <leithdm@gmail.com>
2021-08-22 19:59:46 +0100

    feat: Add customer info to Order and OrderItems. Register Order and OrderItem with admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,7 +1,52 @@
 from django.contrib import admin
 from store.models import Category, Product
-from .models import Cart, CartItem
+from .models import Cart, CartItem, Order, OrderItem
 
 # Register your models here.
 admin.site.register(Cart)
 admin.site.register(CartItem)
+
+class OrderItemAdmin(admin.TabularInline):
+    model = OrderItem
+    fieldsets = [
+        ('Product', {'fields': ['product'], }),
+        ('Quantity', {'fields': ['quantity'], }),
+        ('Price', {'fields': ['price'], }),
+    ]
+    readonly_fields = ['product', 'quantity', 'price']
+    #disable the delete option
+    can_delete = False
+    #delete empty rows in admin UI
+    max_num = 0
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'billingName', 'emailAddress', 'created']
+    list_display_links = ('id', 'billingName')
+    search_fields = ['id', 'billingName', 'emailAddress']
+    #readonly as we don't want to adjust orders that have been processed
+    readonly_fields = ['id', 'token', 'total', 'emailAddress', 'created',
+                       'billingName', 'billingAddress1', 'billingCity', 'billingPostcode',
+                       'billingCountry', 'shippingName', 'shippingAddress1', 'shippingCity',
+                       'shippingPostcode', 'shippingCountry']
+
+    fieldsets = [
+        ('ORDER', {'fields': ['id', 'token', 'total', 'created']}),
+        ('BILLING', {'fields': ['billingName', 'billingAddress1',
+                                            'billingCity', 'billingPostcode', 'billingCountry', 'emailAddress']}),
+        ('SHIPPING', {'fields': ['shippingName', 'shippingAddress1',
+                                             'shippingCity', 'shippingPostcode', 'shippingCountry']}),
+    ]
+
+    inlines = [
+        OrderItemAdmin,
+    ]
+
+    #disable the delete button
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    #disable add button
+    def has_add_permission(self, request):
+        return False
--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from store.models import Category, Product
-from .models import Cart, CartItem
+from .models import Cart, CartItem, Order, OrderItem
 from django.core.exceptions import ObjectDoesNotExist
 from django.conf import settings
 import stripe
@@ -71,6 +71,16 @@ def cart_detail(request, total=0, counter=0, cart_items=None):
         try:
             token = request.POST['stripeToken']
             email = request.POST['stripeEmail']
+            billingName = request.POST['stripeBillingName']
+            billingAddress1 = request.POST['stripeBillingAddressLine1']
+            billingCity = request.POST['stripeBillingAddressCity']
+            billingCountry = request.POST['stripeBillingAddressCountryCode']
+            shippingName = request.POST['stripeShippingName']
+            shippingAddress1 = request.POST['stripeShippingAddressLine1']
+            shippingCity = request.POST['stripeShippingAddressCity']
+            shippingCountry = request.POST['stripeShippingAddressCountryCode']
+            billingPostcode = request.POST['stripeBillingAddressZip']
+            shippingPostcode = request.POST['stripeShippingAddressZip']
             customer = stripe.Customer.create(
                 email=email,
                 source=token
@@ -81,6 +91,43 @@ def cart_detail(request, total=0, counter=0, cart_items=None):
                 description=description,
                 customer=customer.id
             )
+            try:
+                order_details = Order.objects.create(
+                    token=token,
+                    total=total,
+                    emailAddress=email,
+                    billingName=billingName,
+                    billingAddress1=billingAddress1,
+                    billingCity=billingCity,
+                    billingPostcode=billingPostcode,
+                    billingCountry=billingCountry,
+                    shippingName=shippingName,
+                    shippingAddress1=shippingAddress1,
+                    shippingCity=shippingCity,
+                    shippingPostcode=shippingPostcode,
+                    shippingCountry=shippingCountry
+                )
+                #after receiving order, save to the database
+                order_details.save()
+                for order_item in cart_items:
+                    or_item = OrderItem.objects.create(
+                        product=order_item.product.name,
+                        quantity=order_item.quantity,
+                        price=order_item.product.price,
+                        order=order_details
+                    )
+                    or_item.save()
+
+                    #reduce the stock that we have
+                    products = Product.objects.get(id=order_item.product.id)
+                    products.stock = int(order_item.product.stock - order_item.quantity)
+                    products.save()
+                    order_item.delete()
+                    print('**The order has been successfully created**')
+                return redirect('home')
+            #if the object does not exist, simply do a pass to ignore it
+            except ObjectDoesNotExist:
+                pass
         except stripe.error.CardError as e:
             return False, e
     return render(request, 'checkout/index.html', dict(cart_items=cart_items, total=total, counter=counter, data_key=data_key, stripe_total=stripe_total, description=description))
