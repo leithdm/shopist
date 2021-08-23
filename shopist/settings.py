869e86bdf7a0a10eca1ea856d439302ed79872a7
leithdm@gmail.com <leithdm@gmail.com>
2021-08-23 20:59:24 +0100

    feat: Remove secret key and set debug

--- shopist/settings.py
+++ shopist/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uo0xlpgp0%vy877l7c$xo$@-^37nf-*fpzpdqf)py1tr_puyng'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['shopist-dml.herokuapp.com', 'localhost']
 
