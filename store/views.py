b8ff5d2b712dfbc55a5d4818192d54e21c537425
leithdm@gmail.com <leithdm@gmail.com>
2021-08-25 20:42:40 +0100

    feat: Add pagination to store index page

--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product, Review
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
+
 
 # Create your views here.
 def home(request, category_slug=None):
@@ -13,6 +15,18 @@ def home(request, category_slug=None):
     else:
         # if the category field is empty, display all available products
         products = Product.objects.all().filter(available=True)
+
+    paginator = Paginator(products, 4)
+
+    try:
+        page = int(request.GET.get('page', '1'))
+    except:
+        page = 1
+
+    try:
+        products = paginator.page(page)
+    except(EmptyPage, InvalidPage):
+        products = paginator.page(paginator.num_pages)
 
     return render(request, 'store/index.html', {'category': category_page, 'products': products})
 
