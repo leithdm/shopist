d7c57c3c3da1f88049568ee39047718d1ad341c4
leithdm@gmail.com <leithdm@gmail.com>
2021-09-05 18:43:08 +0100

    feat: Ability to delete reviews from profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from checkout.models import Order, OrderItem
+from store.models import Review
 from django.core.exceptions import ObjectDoesNotExist
 from django.conf import settings
 from django.contrib.auth.models import Group, User
@@ -14,9 +15,8 @@ def profile(request):
     if request.user.is_authenticated:
         email = str(request.user.email)
         order_details = Order.objects.filter(emailAddress=email)
-        print(email)
-        print(order_details)
-    return render(request, 'profiles/profile.html', {'order_details': order_details})
+        reviews = Review.objects.filter(user=request.user)    
+    return render(request, 'profiles/profile.html', {'order_details': order_details, 'reviews': reviews})
 
 
 @login_required(redirect_field_name='next', login_url='login')
@@ -26,3 +26,12 @@ def viewOrder(request, order_id):
         order = Order.objects.get(id=order_id, emailAddress=email)
         order_items = OrderItem.objects.filter(order=order)
     return render(request, 'profiles/order_detail.html', {'order': order, 'order_items': order_items})
+
+
+@login_required(redirect_field_name='next', login_url='login')
+def deleteReview(request, review_id):
+    if request.user.is_authenticated:
+        review_to_delete = Review.objects.get(id=review_id)
+        review_to_delete.delete()
+        reviews = Review.objects.filter(user=request.user)    
+    return render(request, 'profiles/profile.html', {'reviews': reviews})
